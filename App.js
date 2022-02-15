cc151aa1ae342cff62560befcaa5ae6159eec9c5
Marina Doroshuk <59647513+maridoroshuk@users.noreply.github.com>
2022-02-15 08:13:03 +0300

    added code

---
+++ App.js
@@ -0,0 +1,45 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
+import Filter from "./components/Filter";
+import { motion, AnimatePresence } from "framer-motion";
+
+function App() {
+  const [popular, setPopular] = useState([]);
+  const [filtered, setFiltered] = useState([]);
+  const [activeGenre, setActiveGenre] = useState(0);
+
+  useEffect(() => {
+    fetchPopular();
+  }, []);
+
+  const fetchPopular = async () => {
+    const data = await fetch(
+      " https://api.themoviedb.org/3/movie/popular?api_key=349f5f043e5262f4f0acbcb422997c26&language=en-US&page=1"
+    );
+    const movies = await data.json();
+    setPopular(movies.results);
+    setFiltered(movies.results);
+  };
+
+  return (
+    <div className="App">
+      <Filter
+        popular={popular}
+        setFiltered={setFiltered}
+        activeGenre={activeGenre}
+        setActiveGenre={setActiveGenre}
+      />
+      <motion.div layout className="popular-movies">
+        <AnimatePresence>
+          {filtered.map((movie) => {
+            return <Movie key={movie.id} movie={movie} />;
+          })}
+        </AnimatePresence>
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
